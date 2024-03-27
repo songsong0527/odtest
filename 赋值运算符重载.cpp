5a695557694832241248e77cf169bd05035bce64
QGY <875397147@qq.com>
2024-03-27 17:48:41 +0800

    赋值运算符重载（未完成）

--- 赋值运算符重载.cpp
+++ 赋值运算符重载.cpp
@@ -1,4 +1,19 @@
 #include <iostream>
 using namespace std;
 
-
+class Person {
+public:
+	Person(int a) {
+		m_a = new int(a);
+	}
+	int* m_a;
+};
+void test() {
+	Person p1(20);
+	Person p2(18);
+	p2 = p1;
+	cout << *p2.m_a << endl<<*p1.m_a << endl << &p2 << endl << &p1 << endl << &p2.m_a << endl << &p1.m_a;
+}
+int main() {
+	test();
+}
--- 递增运算符重载.cpp
+++ 递增运算符重载.cpp
@@ -1,29 +1,29 @@
-#include <iostream>
-using namespace std;
-
-class Model
-{
-public:
-	int a = 0;
-};
-
-//前置递增
-int operator++(Model& model1) {
-	model1.a += 1;
-	return model1.a;
-}
-//后置递增
-int operator++(Model& model1,int/*占位符*/) {
-	int temp = model1.a;
-	model1.a++;
-	return temp;
-}
-
-int main() {
-	Model model1;
-	cout << ++model1 << endl;
-
-	cout << model1++ << endl;
-	
-	cout << model1.a << endl;
-}
+//#include <iostream>
+//using namespace std;
+//
+//class Model
+//{
+//public:
+//	int a = 0;
+//};
+//
+////前置递增
+//int operator++(Model& model1) {
+//	model1.a += 1;
+//	return model1.a;
+//}
+////后置递增
+//int operator++(Model& model1,int/*占位符*/) {
+//	int temp = model1.a;
+//	model1.a++;
+//	return temp;
+//}
+//
+//int main() {
+//	Model model1;
+//	cout << ++model1 << endl;
+//
+//	cout << model1++ << endl;
+//	
+//	cout << model1.a << endl;
+//}
